4d4fc0fd71e5e838a743293351f4b4ccde92ea2f
rockske8er <rockske8er@gmail.com>
2023-09-22 02:06:13 -0300

    fix:create a footer for bk website

--- src/app/lp/bk/components/Container/index.tsx
+++ src/app/lp/bk/components/Container/index.tsx
@@ -1,12 +1,18 @@
-import { FC, ReactNode } from 'react'
+import { cn } from '@/lib/utils'
+import { FC, HTMLAttributes, ReactNode } from 'react'
 
-type ContainerProps = {
+type ContainerProps = HTMLAttributes<HTMLDivElement> & {
   children: ReactNode
 }
 
-export const Container: FC<ContainerProps> = ({ children }) => {
+export const Container: FC<ContainerProps> = ({ children, className }) => {
   return (
-    <div className="w-full max-w-[1191px] mx-auto px-4 flex items-center justify-between">
+    <div
+      className={cn(
+        'w-full max-w-[1191px] mx-auto px-4 flex items-center justify-between',
+        className,
+      )}
+    >
       {children}
     </div>
   )
--- src/app/lp/bk/components/Footer/index.tsx
+++ src/app/lp/bk/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react'
 import Link from 'next/link'
 import { Container } from '../Container'
 import Image from 'next/image'
@@ -10,10 +11,10 @@ import twitterIcon from '@/assets/bk/icon-twitter .svg'
 
 import bkLogo from '@/assets/bk/logo-bk.svg'
 
-export function Footer() {
+export const Footer: FC = () => {
   return (
     <footer className="w-full flex flex-col bg-bk-brown items-center justify-center">
-      <Container>
+      <Container className={'pt-10 pb-24 flex flex-col sm:flex-row'}>
         <ul className="flex items-center gap-9 text-bk-white font-mono">
           <li>
             <Link href={'#'}>Fale conosco</Link>
@@ -32,25 +33,38 @@ export function Footer() {
           </li>
         </ul>
 
-        <Image alt="" src={bkLogo} />
+        <div className="">
+          <Image alt="" src={bkLogo} width={96} height={96} />
+        </div>
 
         <ul className="flex items-center gap-8">
-          <span>Acompanhe nas redes sociais</span>
+          <span className="text-bk-white font-mono">
+            Acompanhe nas redes sociais
+          </span>
           <div className=" flex items-center gap-6">
             <li>
-              <Link href={'#'}>
+              <Link
+                href={'#'}
+                className="w-6 h-6 flex items-center justify-center"
+              >
                 <Image alt="" src={instagramIcon} />
               </Link>
             </li>
 
             <li>
-              <Link href={'#'}>
+              <Link
+                href={'#'}
+                className="w-6 h-6 flex items-center justify-center"
+              >
                 <Image alt="" src={facebookIcon} />
               </Link>
             </li>
 
             <li>
-              <Link href={'#'}>
+              <Link
+                href={'#'}
+                className="w-6 h-6 flex items-center justify-center"
+              >
                 <Image alt="" src={twitterIcon} />
               </Link>
             </li>
